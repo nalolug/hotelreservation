710b94c8418af9ce2f6903dc193866ae72fad07b
narenmac <31804697+narenmac@users.noreply.github.com>
2023-02-06 13:41:48 +0530

    added basic city search query

--- hotelreservation/src/main/java/learning/systemdesign/hotelreservation/repository/HotelReservationRepository.java
+++ hotelreservation/src/main/java/learning/systemdesign/hotelreservation/repository/HotelReservationRepository.java
@@ -2,12 +2,13 @@ package learning.systemdesign.hotelreservation.repository;
 
 import learning.systemdesign.hotelreservation.model.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.List;
-import java.util.Optional;
 
 @EnableJpaRepositories
 public interface HotelReservationRepository extends JpaRepository<Hotel, Integer> {
+    @Query(value = "SELECT h FROM Hotel h WHERE h.city = ?1")
     List<Hotel> findByCity(String city);
 }
